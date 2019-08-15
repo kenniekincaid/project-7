aa6664551df5e03b7ded569cc4b13c8b5c8ead1a
Kendra Roberson <kendra.roberson@verizon.com>
2019-08-15 18:43:49 -0500

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,8 @@
+//Apply for non-commercial API key from Tumblr and enter that key below.
+
+/** React Gallery App
+Key: 4cca5f091fccb7ac8537bb685969bcde
+Secret: 11276d7e10b5b46d*/
+
+const apiKey = '4cca5f091fccb7ac8537bb685969bcde';
+export default apiKey;
